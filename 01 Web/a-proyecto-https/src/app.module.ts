4fba78b07f4b5440bd25ed145730e582f03c2b45
daniel <daniel.samaniego@epn.edu.ec>
2019-11-29 16:07:10 -0500

    dbeaver

--- 01 Web/a-proyecto-https/src/app.module.ts
+++ 01 Web/a-proyecto-https/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot(
+          {
+            type: 'mysql',
+            host: 'localhost',
+            port: 32793,
+            username: 'daniel',
+            password: '1234',
+            database: 'web',
+            entities: [
+                UsuarioEntity,
+            ],
+            synchronize: true,   // Crear -> true, Conectar -> False
+          },
+      ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ 01 Web/a-proyecto-https/src/usuario/usuario.entity.ts
@@ -0,0 +1,13 @@
+import {Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('usuario_web')
+export class UsuarioEntity {
+    @PrimaryGeneratedColumn({
+        type: 'int',
+        unsigned: true,
+        name: 'id_web',
+        comment: 'Identificador de la tabla usuario',
+    })
+    id: number;
+}
+
